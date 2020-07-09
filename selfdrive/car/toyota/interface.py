1673f70580c6fc5c7a5070fc8173296576209bb1
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2020-07-09 19:28:25 +0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -312,8 +312,8 @@ class CarInterface(CarInterfaceBase):
         max_bp = 0.
       ret.gasMaxBP = [max_bp]
       ret.gasMaxV = [0.5]
-      ret.longitudinalTuning.kpV = [2.65, 1.6, 1.3]
-      ret.longitudinalTuning.kiV = [0.54, 0.335]
+      ret.longitudinalTuning.kpV = [2.6, 1.65, 1.3]
+      ret.longitudinalTuning.kiV = [0.54, 0.33]
 
     return ret
 
