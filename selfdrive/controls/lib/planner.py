04da4f486de0be0e0cf3d3fd40cbd8da6840f71d
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2020-10-03 19:06:32 +0800

    Update planner.py

--- selfdrive/controls/lib/planner.py
+++ selfdrive/controls/lib/planner.py
@@ -45,10 +45,10 @@ DP_ECO = 1
 DP_NORMAL = 2
 DP_SPORT = 3
 # accel profile by @arne182
-_DP_CRUISE_MIN_V = [-2.0, -1.5, -1.0, -0.7, -0.5]
-_DP_CRUISE_MIN_V_ECO = [-1.0, -0.7, -0.6, -0.5, -0.3]
+_DP_CRUISE_MIN_V = [-1.6, -1.2, -1.0, -0.8, -0.6]
+_DP_CRUISE_MIN_V_ECO = [-0.9, -0.7, -0.6, -0.5, -0.3]
 _DP_CRUISE_MIN_V_SPORT = [-3.0, -2.6, -2.3, -2.0, -1.0]
-_DP_CRUISE_MIN_V_FOLLOWING = [-4.0, -4.0, -3.5, -2.5, -2.0]
+_DP_CRUISE_MIN_V_FOLLOWING = [-3.0, -3.0, -3.5, -2.5, -2.0]
 _DP_CRUISE_MIN_BP = [0.0, 5.0, 10.0, 20.0, 55.0]
 
 _DP_CRUISE_MAX_V = [1.95, 1.25, 0.87, 0.37, 0.19]
